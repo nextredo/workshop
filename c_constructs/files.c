3ff17db01e0ad03d2cc0ddf68e90cdab030f055c
nextredo <kb7737@gmail.com>
2025-03-28 23:23:32 +1100

    feat(c): add file ptr check

---
+++ c_constructs/files.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// https://en.cppreference.com/w/c/io/fclose
+
+// Suppress specific clang-tidy warnings
+// NOLINTBEGIN(readability-*, cppcoreguidelines-*, hicpp-*)
+
+int main()
+{
+    // TODO use tmpnam(NULL) instead
+    const char* fname = "/tmp/unique_name.txt";
+
+    FILE* fp = fopen(fname, "w+");
+    if (!fp) {
+        perror("File opening failed");
+        return EXIT_FAILURE;
+    }
+
+    printf("File ptr before closure: %p\n", fp);
+    int closed = fclose(fp);
+    printf("Closed w/ exit code: %d\n", closed);
+    printf("File ptr after closure: %p\n", fp);
+
+    return EXIT_SUCCESS;
+}
+
+// NOLINTEND(readability-*, cppcoreguidelines-*, hicpp-*)
