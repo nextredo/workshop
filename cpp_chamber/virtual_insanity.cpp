efd33f10877d03729e48d8dff0af877db21eec73
nextredo <kb7737@gmail.com>
2025-03-30 12:11:37 +1100

    refactor(cpp): combine virtuals tests

--- cpp_chamber/virtual_insanity.cpp
+++ cpp_chamber/virtual_insanity.cpp
@@ -1,29 +1,50 @@
 #include <iostream>
 
-/// @brief Simple base object
-struct Base
+/// @brief Simple base class
+class Base
 {
+private:
 public:
-    int mInt = 4;
-    virtual void doSomething() { std::cout << "base vfunc\n"; }
+    /// @brief Standard constructor
+    Base() { std::cout << "base ctor\n"; }
 
-    Base()          { std::cout << "base ctor\n"; }
+    /// @brief Copy/move constructors
+    Base(const Base &)                = delete;
+    Base &operator=(const Base &)     = delete;
+    Base(Base &&) noexcept            = delete;
+    Base &operator=(Base &&) noexcept = delete;
+
+    /// @brief Standard destructor
     virtual ~Base() { std::cout << "base dtor\n"; }
+
+    /// @brief Stub method
+    [[nodiscard]] virtual int doSomething()
+    {
+        std::cout << "base vfunc\n";
+        return 0;
+    }
 };
 
-/// @brief Simple derived object
-struct Derived : public Base
-{
-    int mInt = 4;
-    void doSomething() override { std::cout << "derived vfunc\n"; }
 
+/// @brief Simple derived class
+class Derived : public Base
+{
+private:
+public:
     Derived()           { std::cout << "derived ctor\n"; }
     ~Derived() override { std::cout << "derived dtor\n"; }
-};
 
+    /// @brief Overidden method
+    int doSomething() final
+    {
+        std::cout << "derived vfunc\n";
+        return 1;
+    }
+};
 
 int main()
 {
+    std::cout << "-------------------\n";
     {
         Derived der;
         der.doSomething();
@@ -33,6 +54,24 @@ int main()
 
         // class destructor runs here
     }
+    std::cout << "-------------------\n";
+
+    std::cout << "-------------------\n";
+    {
+        // WARN: NODISCARD DOESN'T PROPAGATE!!
+        Derived deriv;
+        deriv.doSomething();
+
+        // NOTE: Discarded return here is picked up by clang-tidy and g++
+        Base based;
+        based.doSomething();
+
+        // NOTE: Discarded return is NOT picked up here by g++
+        // it is by clang-tidy though
+        auto& alias = static_cast<Base&>(deriv);
+        alias.doSomething();
+    }
+    std::cout << "-------------------\n";
 
     return 0;
 }
