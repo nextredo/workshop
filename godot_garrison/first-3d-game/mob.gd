5d0122f952517c706560ae76a3cc831aa7cfb884
nextredo <kb7737@gmail.com>
2025-07-29 19:07:45 +1000

    refactor(mob): add and use animation

--- godot_garrison/first-3d-game/mob.gd
+++ godot_garrison/first-3d-game/mob.gd
@@ -4,10 +4,10 @@ extends CharacterBody3D
 signal squashed
 
 # Minimum speed of the mob [m/s]
-@export var min_speed = 10
+@export var min_speed := 10
 
 # Maximum speed of the mob [m/s]
-@export var max_speed = 18
+@export var max_speed := 18
 
 
 func _physics_process(_delta: float) -> void:
@@ -24,13 +24,16 @@ func initialise(start_pos: Vector3, player_pos: Vector3) -> void:
 	rotate_y(randf_range(-PI/4, PI/4))
 
 	# Calculate random (integer) speed
-	var rand_speed = randi_range(min_speed, max_speed)
+	var rand_speed := randi_range(min_speed, max_speed)
 
 	# Calc fwd velocity (speed)
 	velocity = Vector3.FORWARD * rand_speed
 
 	# Then rotate it to move in dir the mob is looking
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+	# Set a random animation speed
+	$AnimationPlayer.speed_scale = rand_speed / min_speed
 
 
 func squash() -> void:
