ccf91c8afeb8b258fedbd139ea2ed277da1da4d1
nextredo <kb7737@gmail.com>
2025-09-01 23:11:16 +1000

    refactor(rust): add multiple guesses functionality

--- rust_range/the-book-notes/02-guessing-game/guessing_game/src/main.rs
+++ rust_range/the-book-notes/02-guessing-game/guessing_game/src/main.rs
@@ -18,74 +18,80 @@ fn main() {
     let secret = rand::thread_rng().gen_range(1..=100);
     println!("Secret no is {secret:?}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // Define new variable
-        // Mutation
-            // Vars are immutable by default
-            // Make it mutable with the `mut` keyword
-        // `=`
-            // Bind var to something
-        // String
-            // Growable, UTF-8 encoded text
-            // Heap-allocated
-        // `::`
-            // Notes that the `new` function is associated with the `String` type
-        // `new`
-            // Common name for functions that make a new value
-    let mut guess = String::new();
+        // Define new variable
+            // Mutation
+                // Vars are immutable by default
+                // Make it mutable with the `mut` keyword
+            // `=`
+                // Bind var to something
+            // String
+                // Growable, UTF-8 encoded text
+                // Heap-allocated
+            // `::`
+                // Notes that the `new` function is associated with the `String` type
+            // `new`
+                // Common name for functions that make a new value
+        let mut guess = String::new();
 
-    // Broken up across a few lines for readability
-        // Newlines on new methods by convention
-    // `io::stdin()`
-        // Can use through `std::io::stdin()` without the `use std::io`
-        // Returns a `std::io::Stdin` instance
-    // `.read_line()`
-        // Appends stdin to a string
-        // `&mut guess`
-            // `&` indicates this is arg is a *reference*
-            // References are immutable by default
-            // Make it mutable with the `mut` keyword
-            // Same behaviour as vars ^^
-    // `.expect()`
-        // `.read_line()` returns a `Result`
-            // `Result` is an enumeration/enum
-            // Each value it can take is called a variant
-            // Can be `Ok` or `Err`
-            // `Ok` contains the value (if it succeeded)
-            // `Err` contains error info (if it failed)
-        // Will return the value of the result if `Result` is `Ok`
-        // Returns number of bytes in the input in our case
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line :(");
+        // Broken up across a few lines for readability
+            // Newlines on new methods by convention
+        // `io::stdin()`
+            // Can use through `std::io::stdin()` without the `use std::io`
+            // Returns a `std::io::Stdin` instance
+        // `.read_line()`
+            // Appends stdin to a string
+            // `&mut guess`
+                // `&` indicates this is arg is a *reference*
+                // References are immutable by default
+                // Make it mutable with the `mut` keyword
+                // Same behaviour as vars ^^
+        // `.expect()`
+            // `.read_line()` returns a `Result`
+                // `Result` is an enumeration/enum
+                // Each value it can take is called a variant
+                // Can be `Ok` or `Err`
+                // `Ok` contains the value (if it succeeded)
+                // `Err` contains error info (if it failed)
+            // Will return the value of the result if `Result` is `Ok`
+            // Returns number of bytes in the input in our case
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line :(");
 
-    // Variable shadowing (2nd thing named "guess")
-    // Often used when converting between types
-    // More in chapter 3
-    // `.trim()`
-        // Removes leading/trailing whitespace
-    // `.parse()`
-        // Uses the u32 type annotation to parse the string into that type
-    // Rust will infer the type of `secret` to be `u32` like this var
-        // Since they get compared together later
-    let guess: u32 = guess.trim()
-        .parse()
-        .expect("Please type a number");
+        // Variable shadowing (2nd thing named "guess")
+        // Often used when converting between types
+        // More in chapter 3
+        // `.trim()`
+            // Removes leading/trailing whitespace
+        // `.parse()`
+            // Uses the u32 type annotation to parse the string into that type
+            // Returns `Result`, so it can signal failure
+        // Rust will infer the type of `secret` to be `u32` like this var
+            // Since they get compared together later
+        let guess: u32 = guess.trim()
+            .parse()
+            .expect("Please type a number");
 
-    // Macro, `{}` is a placeholder
-    // Can put var name directly in them
-    // Or do comma stuff after the string (w/ empty `{}`s)
-    println!("You guessed: {guess}");
+        // Macro, `{}` is a placeholder
+        // Can put var name directly in them
+        // Or do comma stuff after the string (w/ empty `{}`s)
+        println!("You guessed: {guess}");
 
-    // `match` statement consists of *arms*
-    // Arms consist of a *pattern* and the code should run if that pattern matches
-    // Rust looks thru arms in turn
-    // `match` covered further in chapters 6, 19
-    // Useful for ensuring your code covers all possible paths
-    match guess.cmp(&secret) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // `match` statement consists of *arms*
+        // Arms consist of a *pattern* and the code should run if that pattern matches
+        // Rust looks thru arms in turn
+        // `match` covered further in chapters 6, 19
+        // Useful for ensuring your code covers all possible paths
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
