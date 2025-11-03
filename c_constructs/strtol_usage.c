96fa32ddfc05187c45b63d0ee74ace86378a0aec
nextredo <kb7737@gmail.com>
2025-11-03 11:32:35 +1100

    refactor(strtol): cleanup

--- c_constructs/strtol_usage.c
+++ c_constructs/strtol_usage.c
@@ -3,25 +3,37 @@
 #include <errno.h>
 
 #define BUF_LEN 64
+#define FILEPATH "/tmp/hehe"
 
-int main()
-{
+/* Expecting a basic file that looks like this:
+ * key:    value
+ * key:    value
+ * key:    value
+ * key:    value
+ */
+
+int main() {
     errno = 0;
-    FILE* fp = fopen("/tmp/hehe", "r");
-    if (!fp)
+    FILE* file = fopen(FILEPATH, "r");
+    if (!file) {
+        perror("Bad fopen");
         return EXIT_FAILURE;
-
+    }
 
     char buffer[BUF_LEN];
-    char* ptr = fgets(buffer, BUF_LEN, fp);
-    if (!ptr)
+    char* ptr = fgets(buffer, BUF_LEN, file);
+    if (!ptr) {
+        perror("Bad fgets");
         return EXIT_FAILURE;
+    }
 
+    // NOTE: scanf version below. Bad since it doesn't report conversion errors
+    // Alternatively, do strstr() and strtol() checking instead
     long bing = 0;
+    errno = 0;
     int ret = sscanf(buffer, "bing: %ld", &bing);
-    if (ret != 1)
-    {
-        printf("what\n");
+    if (ret != 1) {
+        perror("Bad sscanf");
         return EXIT_FAILURE;
     }
 
